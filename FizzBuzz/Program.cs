b1c633080456997c696626da1c30957dcd403468
Nina <boldurfaith@gmail.com>
2023-04-06 16:15:47 +0100

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FizzBuzz(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 });
+        }
+
+        private static void FizzBuzz(int[] arrayNumbers)
+        {
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                if (arrayNumbers[i] % 3 == 0 && (arrayNumbers[i] % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (arrayNumbers[i] % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (arrayNumbers[i] % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{arrayNumbers[i]}");
+                }
+
+            }
+        }
+    }
+}
